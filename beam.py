6e20c3213795e214a0f74d59b87bdbdacddc0204
mrunalsompura1995 <mrunalsompura@gmail.com>
2021-11-09 15:01:08 +0100

    not faster

--- beam.py
+++ beam.py
@@ -5,24 +5,19 @@ def current_milli_time():
     return round(time.time() * 1000)
 
 def checkBetween(a,b,c):
+    # checks if b is in between a and c
     ac,bc,ab = checkDistAngle(a,b)[0] ,checkDistAngle(b,c)[0] , checkDistAngle(a,c)[0]
     if ac + bc - ab < 0.0000001:
         return True
     return False 
     
 def checkDistAngle(point1,point2):
+    # distance and angle between point1 and point2
     x1,y1 = point1  
     x2,y2 = point2
     dist = round((abs(x2 - x1)** 2 + abs(y2 - y1)** 2)** 0.5,10)
     angle = round(math.atan2((y2 - y1),(x2 - x1)),10)
     return dist,angle
-
-def getAngleList(your_pos,list):
-    AngList = []
-    for l in list:
-        dist,ang = checkDistAngle(your_position,l)
-        AngList.append(ang)
-    return AngList
     
 def solution(dimensions, your_position, trainer_position, distance):
     count = 0
@@ -32,7 +27,9 @@ def solution(dimensions, your_position, trainer_position, distance):
 
     posX, posY , posX0, posY0 = [x],[y],[x0],[y0]
     posx, posy, posx0, posy0 = x,y,x0,y0
+    posXY,posXY0 = [(x,y)],[(x0,y0)]
     candidate, yourpos = [],[]
+    list1 = []
     
     n = max(w,h,distance)
     for r in range(100):
@@ -54,22 +51,20 @@ def solution(dimensions, your_position, trainer_position, distance):
 
     trainer = [[candidate.extend(((i,j),(i,-j),(-i,j),(-i,-j))) for i in posX] for j in posY]
     you = [[yourpos.extend(((i,j),(i,-j),(-i,j),(-i,-j))) for i in posX0] for j in posY0]
+    
     newlist = []
-
     for i in range(len(candidate)):
         newlist.append(candidate[i])
         newlist.append(yourpos[i])
-    selected_cand = []
-    for cand in newlist:
-        if 0 < checkDistAngle(cand,your_position)[0] <= distance:
-            selected_cand.append(cand)
-        
+    
+    selected_cand = [cand for cand in newlist if 0 < checkDistAngle(cand,your_position)[0] <= distance]
+    
     HitList = []
     HitAngle = []
     
     for can in selected_cand:
         ang = checkDistAngle(can,your_position)[1]
-        if not checkBetween(trainer_position,your_position,can) and ang not in HitAngle:# and checkCorner(i):
+        if not checkBetween(trainer_position,your_position,can) and ang not in HitAngle:
             HitAngle.append(ang)
             HitList.append(can)
             if can in candidate:
@@ -99,9 +94,10 @@ def solutions(dimensions, your_position, trainer_position, distance):
                 # mark self-hits with a negative travel so we can filter later
                 hits[bearing] = travel * (-1 if position == your_position else 1)
     return len([1 for travel in hits.values() if travel > 0])
-# dimensions = [3,3]
-# your_position = [1,1]
-# trainer_position = [2,2]
+
+dimensions = [3,3]
+your_position = [1,1]
+trainer_position = [2,2]
 # distance = 10
 
 # dimensions = [2,5]
@@ -114,9 +110,9 @@ def solutions(dimensions, your_position, trainer_position, distance):
 # trainer_position = [180, 100]
 # distance = 500
 
-dimensions = [9,9]
-your_position = [3,3]
-trainer_position = [6,6]
+# dimensions = [9,9]
+# your_position = [3,3]
+# trainer_position = [6,6]
 # distance = 10
 
 # dimensions = [10,10]
@@ -133,7 +129,6 @@ trainer_position = [6,6]
 # your_position = [1, 1]
 # trainer_position = [2, 1]
 for d in range(100):
-    distance = d
     t1 = current_milli_time()
     test = solution(dimensions,your_position,trainer_position,d)
     t2 = current_milli_time()
